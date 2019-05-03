0798bdc0e6e8f3eae0fdd74d10a78124a2a10506
Artem Shevchenko <shevartsoft@gmail.com>
2019-05-03 22:13:49 +0300

    base-arch - work with base classes:  add splash bg drawable and theme

--- presentation/src/main/java/com/shevart/rocketlaunches/screen/home/MainActivity.kt
+++ presentation/src/main/java/com/shevart/rocketlaunches/screen/home/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.shevart.rocketlaunches
+package com.shevart.rocketlaunches.screen.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shevart.rocketlaunches.R.layout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
 
     }
 }
