87895e8a340930ceceea73bf3316b4b63b178faa
Artem Shevchenko <shevartsoft@gmail.com>
2019-05-11 01:36:48 +0300

    launches_list - display real images of launches in list

--- presentation/src/main/java/com/shevart/rocketlaunches/models/mapper/UILaunchMapper.kt
+++ presentation/src/main/java/com/shevart/rocketlaunches/models/mapper/UILaunchMapper.kt
@@ -1,7 +1,9 @@
 package com.shevart.rocketlaunches.models.mapper
 
+import com.shevart.domain.contract.app.LaunchesList.DEFAULT_LIST_IMAGE_RESOLUTION
 import com.shevart.domain.contract.mapper.Mapper
 import com.shevart.domain.models.launch.LaunchStatus
+import com.shevart.domain.models.launch.MediaInfo
 import com.shevart.domain.models.launch.RocketLaunch
 import com.shevart.rocketlaunches.core.resources.ResourceProvider
 import com.shevart.rocketlaunches.core.resources.StringProvider
@@ -29,6 +31,21 @@ class UILaunchMapper
             countryNameResId = stringProvider.getCountryNameResId(from.country),
             favoritesIconResId = resourceProvider.getFavoriteResId(from.favorite),
             date = from.date.toString(), // todo map date
-            status = launchStatusMapper.map(from.status)
+            status = launchStatusMapper.map(from.status),
+            imageUrl = from.rocket.mediaInfo.obtainImageUrl()
         )
+
+    /**
+     * This is bad idea to put logic of image selection to mapper.
+     * But if have no time for do it in more right way. I think this work must do viewModel or
+     * useCase
+     *
+     * So there is todo - replace this logic to right place
+     */
+    private fun MediaInfo.obtainImageUrl(): String? {
+        return images.find { candidate ->
+            candidate.resolution == DEFAULT_LIST_IMAGE_RESOLUTION
+                    && !candidate.imageUrl.contains("placeholder")
+        }?.imageUrl
+    }
 }
