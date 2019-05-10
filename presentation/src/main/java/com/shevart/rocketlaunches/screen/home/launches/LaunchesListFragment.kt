e0338a776d5c0b35c4f551dc29c79c8baec64573
Artem Shevchenko <shevartsoft@gmail.com>
2019-05-11 02:48:43 +0300

    launches_list - clean up, remove test code

--- presentation/src/main/java/com/shevart/rocketlaunches/screen/home/launches/LaunchesListFragment.kt
+++ presentation/src/main/java/com/shevart/rocketlaunches/screen/home/launches/LaunchesListFragment.kt
@@ -2,14 +2,10 @@ package com.shevart.rocketlaunches.screen.home.launches
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.postDelayed
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.transition.TransitionManager
 import com.shevart.rocketlaunches.R
 import com.shevart.rocketlaunches.base.mvvm.AbsMvvmFragment
 import com.shevart.rocketlaunches.di.component.AppComponent
-import com.shevart.rocketlaunches.models.UILaunch
-import com.shevart.rocketlaunches.models.UILaunchStatus
 import com.shevart.rocketlaunches.screen.home.launches.LaunchesListViewModel.State
 import com.shevart.rocketlaunches.screen.home.launches.LaunchesListViewModel.State.*
 import com.shevart.rocketlaunches.screen.shared.launch.LaunchRVAdapter
@@ -46,24 +42,6 @@ class LaunchesListFragment : AbsMvvmFragment<LaunchesListViewModel>() {
         rvLaunches.addOnScrollListener(pagingListEndReachedListener)
 
         observeLiveDataForceNonNull(viewModel.getStateLiveData(), this::renderState)
-
-        // todo remove after test
-        if (true) return
-        rvLaunches.postDelayed(2000L) {
-            TransitionManager.beginDelayedTransition(flLaunchesRoot)
-            evLaunchesError.gone()
-            tvLaunchesTitle.textColorByColorId(R.color.greyDark)
-            ivLaunchesLoading.visible()
-            ivLaunchesLoading.postDelayed(1000L) {
-                showContent()
-            }
-        }
-
-        tvLaunchesTitle.textColorByColorId(R.color.white)
-        evLaunchesError.setBackgroundColorResId(R.color.errorNoInternet)
-        evLaunchesError.setImage(R.drawable.error_no_internet)
-        evLaunchesError.setTitle(R.string.error_no_internet)
-        evLaunchesError.setDescription(R.string.error_no_internet)
     }
 
     private fun renderState(state: State) {
@@ -100,63 +78,4 @@ class LaunchesListFragment : AbsMvvmFragment<LaunchesListViewModel>() {
         evLaunchesError.setTitle(R.string.error_no_internet)
         evLaunchesError.setDescription(R.string.error_no_internet)
     }
-
-    private fun showContent() {
-        ivLaunchesLoading.gone()
-        rvLaunches.visible()
-        adapter.updateItems(getItems())
-    }
-
-    private fun getItems() = listOf(
-        UILaunch(
-            id = 1,
-            name = "Falcon - 1",
-            countryFlagResId = R.drawable.flag_usa,
-            countryNameResId = R.string.country_name_usa,
-            favoritesIconResId = R.drawable.ic_favorite_white,
-            date = "12.22.1999",
-            status = UILaunchStatus(
-                statusResId = R.string.launch_status_successfully,
-                backgroundResId = R.drawable.gradient_status_green
-            ),
-            imageUrl = "https://s3.amazonaws.com/launchlibrary/RocketImages/Falcon1.jpg_720.jpg"
-        ),
-        UILaunch(
-            id = 1,
-            name = "Falcon - 1 by Elon Musk by Tesla and SpaceX",
-            countryFlagResId = R.drawable.flag_usa,
-            countryNameResId = R.string.country_name_usa,
-            favoritesIconResId = R.drawable.ic_favorite_red,
-            date = "12.22.1999",
-            status = UILaunchStatus(
-                statusResId = R.string.launch_status_failed,
-                backgroundResId = R.drawable.gradient_status_red
-            ),
-            imageUrl = "https://s3.amazonaws.com/launchlibrary/RocketImages/Falcon+9+v1.0_720.jpg"
-        ),
-        UILaunch(
-            id = 1,
-            name = "Falcon - 1",
-            countryFlagResId = R.drawable.flag_usa,
-            countryNameResId = R.string.country_name_usa,
-            favoritesIconResId = R.drawable.ic_favorite_white,
-            date = "12.22.1999",
-            status = UILaunchStatus(
-                statusResId = R.string.launch_status_scheduled,
-                backgroundResId = R.drawable.gradient_status_blue
-            )
-        ),
-        UILaunch(
-            id = 1,
-            name = "Falcon - 1",
-            countryFlagResId = R.drawable.flag_usa,
-            countryNameResId = R.string.country_name_usa,
-            favoritesIconResId = R.drawable.ic_favorite_white,
-            date = "12.22.1999",
-            status = UILaunchStatus(
-                statusResId = R.string.launch_status_canceled,
-                backgroundResId = R.drawable.gradient_status_grey_light
-            )
-        )
-    )
 }
